d1bfd3183ecf69b6a5b0bd9aed0d3c890862ad1f
Vinnie <vdreifke@inf.ufsm.br>
2015-10-23 19:38:19 -0200

    d

--- contact_me.php
+++ contact_me.php
@@ -3,8 +3,8 @@
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
    empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   empty($_POST['message'])
+   )
    {
 	echo "No arguments Provided!";
 	return false;
@@ -22,5 +22,5 @@ $email_body = "You have received a new message from your website contact form.\n
 $headers = "From: rtrindade@inf.ufsm.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
-return true;			
+header("Location: index.html");			
 ?>
